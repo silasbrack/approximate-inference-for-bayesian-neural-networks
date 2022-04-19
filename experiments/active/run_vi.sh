178ba5739f61db6b4dc3461d3965d2171179ced9
Silas Brack <s174433@student.dtu.dk>
2022-04-19 16:31:44 +0200

    Fixed active vi.

--- experiments/active/run_vi.sh
+++ experiments/active/run_vi.sh
@@ -6,6 +6,10 @@
 #BSUB -W 24:00
 #BSUB -R "rusage[mem=16GB] span[hosts=1]"
 
+module load python3/3.9.6
+module load cuda/11.3
+source venv/bin/activate
+
 python src/train_active.py \
     --multirun \
     data=mnist \
