f5616cd3c33327097d91ec531b70aa38edcd7854
Silas Brack <s174433@student.dtu.dk>
2022-04-30 21:56:10 +0200

    Fixed multiswag.

--- src/inference/multi_swag.py
+++ src/inference/multi_swag.py
@@ -3,7 +3,6 @@ import os
 import pickle
 import time
 
-import hydra.utils
 import torch
 from torch import nn
 from pyro.infer import Predictive
@@ -37,7 +36,7 @@ class MultiSwag(Inference):
             model_.apply(weight_reset)
             self.ensembles.append(
                 Swag(
-                    hydra.utils.instantiate(model_),
+                    model_,
                     device,
                     swa_start_thresh,
                     posterior_samples,
